f78dca95383ef5906829239337f8b3a04749c419
Saiyan_-Javi <ova22103@uvg.edu.gt>
2026-04-16 18:19:27 -0600

    cambios en el database

--- database.py
+++ database.py
@@ -1,17 +1,20 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# Esto creará un archivo llamado 'ma_jolie.db' en tu carpeta
-SQLALCHEMY_DATABASE_URL = "sqlite:///./ma_jolie.db"
+# Buscamos la URL en la nube. Si estamos en tu compu y no existe, usa el SQLite clásico.
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./ma_jolie.db")
 
-# Configuramos el motor de la base de datos
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# A SQLAlchemy le gusta que el link diga "postgresql://" en vez de "postgres://"
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# Creamos una sesión para poder hacer consultas y guardar datos
+# SQLite necesita un permiso extra ("check_same_thread"), PostgreSQL no.
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Esta clase 'Base' es la que usarán nuestros modelos
 Base = declarative_base()
